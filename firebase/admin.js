eb2effdbf5b24d8897bcc77f47e1eda29e7c084f
Mechi Bakker <mechibakker@gmail.com>
2025-11-23 20:56:57 -0300

    harta

--- firebase/admin.js
+++ firebase/admin.js
@@ -4,13 +4,13 @@ import admin from "firebase-admin";
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY
-        ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
+      projectId: process.env.VERCEL_FIREBASE_PROJECT_ID,
+      clientEmail: process.env.VERCEL_FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.VERCEL_FIREBASE_PRIVATE_KEY
+        ? process.env.VERCEL_FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
         : undefined,
     }),
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    storageBucket: process.env.VERCEL_FIREBASE_STORAGE_BUCKET,
   });
 }
 
